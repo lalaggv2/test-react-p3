e4adc782187208749233cbecb5ce02232b21b7e8
Lala <luangu2020@gmail.com>
2020-08-29 13:53:52 -0400

    new images

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -81,11 +81,6 @@ function Home(props) {
           <h1>{secret}</h1>
         </Col>
       </Row>
-
-      <footer className='footer mt-auto py-3 bg-dark text-white'>
-        <div className='container'><h3>Copyright &copy; 2020 - East Coast Insurance</h3>
-          <h5>Made with love by Angela, Diana, Lynn, Scott and Shannon</h5></div>
-      </footer>
     </Container >
   );
 }
